db750ff8b7404d85e074cef6b64eb8d5e8193b83
EmperorNao <59420696+EmperorNao@users.noreply.github.com>
2022-02-19 16:40:22 +0300

    Utils

---
+++ Distances/distances.py
@@ -0,0 +1,6 @@
+import numpy as np
+
+
+def euclidian_distance(x_1, x_2):
+
+    return np.sum(np.square(x_1 - x_2)) ** 0.5
---
+++ Pipelines/simple.py
@@ -0,0 +1,22 @@
+import numpy as np
+from datasets import train_test_split
+
+
+def pipeline(models: dict, metrics: dict, x, y):
+
+    x_pair, y_pair = train_test_split(x, y, ratio=0.75)
+
+    x_train, x_test = x_pair
+    y_train, y_test = y_pair
+
+    for model in models.values():
+        model.fit(x, y)
+
+    for modelname, model in models.items():
+        y_pred = model.predict(x_test)
+
+        print(f"Model {modelname}")
+        for metricname, metric in metrics.items():
+            print(f"{metricname} = {metric(np.squeeze(y_test, -1), y_pred)}")
+
+        print()
