7274ff8430f21719f65d8b8de7cfd5e8b057d899
EmperorNao <59420696+EmperorNao@users.noreply.github.com>
2022-02-23 12:25:08 +0300

    Added knn exstensions

--- Models/Neighbors.py
+++ Models/Neighbors.py
@@ -4,7 +4,7 @@ from Distances.distances import euclidian_distance
 
 class KNNClassifier:
 
-    def __init__(self, k = 1, dist = euclidian_distance):
+    def __init__(self, k = 1, dist = euclidian_distance, weighted=None, q=0.9, parzen=False, kernel='gauss', fix_h=True, h=None):
 
         self.k = k
         self.dist = dist
@@ -12,6 +12,16 @@ class KNNClassifier:
         self.answ = None
         self.obj = None
         self.classes = None
+        self.weighted = weighted
+
+        self.parzen = parzen
+
+        if kernel == "gauss":
+            self.kernel = lambda x: np.exp(-2 * x * x)
+
+        self.h = h
+        self.fix_h = fix_h
+        self.q = q
 
     def fit(self, x, y):
 
@@ -19,16 +29,77 @@ class KNNClassifier:
         self.answ = y
         self.classes = np.unique(self.answ)
 
-    def predict(self, x):
+    def make_weights(self, size):
+
+        weights = None
+        if not self.weighted:
+            weights = np.ones(size)
+        elif self.weighted == 'linear':
+            weights = np.array([(self.k + 1 - i) / self.k for i in range(1, self.k + 1)])
+        elif self.weighted == 'exponential':
+
+            weights = np.zeros(size)
+            p = self.q
+            for i in range(size):
+                weights[i] = p
+                p *= self.q
+
+        else:
+            raise KeyError("Wrong 'weighted' argument")
+
+        return weights
+
+    def _non_parzen_predict(self, x):
+
+        m = x.shape[0]
+        n = self.obj.shape[0]
+
+        weights = self.make_weights(min(self.k, n))
+        pred = np.zeros(m, dtype='int32')
+        for i, obj in enumerate(x):
+            idx = np.argsort(np.array([self.dist(obj, example) for example in self.obj]))[:min(self.k, n)]
+
+            labels = np.squeeze(self.answ[idx], -1)
+
+            answ = np.zeros(np.unique(self.answ).shape[0])
+            for w, cls in zip(weights, labels):
+                answ[cls] += w
+
+            pred[i] = np.argmax(answ)
+
+        return np.array(pred)
+
+    def _parzen_predict(self, x):
 
         m = x.shape[0]
         n = self.obj.shape[0]
 
         pred = np.zeros(m, dtype='int32')
         for i, obj in enumerate(x):
-            idx = np.argsort(np.array([self.dist(obj, example) for example in self.obj]))[:min(self.k, n)]
+            d = np.array([self.dist(obj, example) for example in self.obj])
+            idx = np.argsort(d)[:min(self.k, n)]
 
             labels = np.squeeze(self.answ[idx], -1)
-            pred[i] = np.bincount(labels).argmax()
+
+            answ = np.zeros(np.unique(self.answ).shape[0])
+
+            if self.fix_h:
+                weights = np.array([self.kernel(dist/self.h) for dist in d[idx]])
+
+            else:
+                last = d[idx][min(self.k, n) - 1]
+                weights = np.array([self.kernel(dist / last) for dist in d[idx]])
+
+            for w, cls in zip(weights, labels):
+                answ[cls] += w
+
+            pred[i] = np.argmax(answ)
 
         return np.array(pred)
+
+    def predict(self, x):
+
+       if self.parzen:
+           return self._parzen_predict(x)
+       else:
+           return self._non_parzen_predict(x)
